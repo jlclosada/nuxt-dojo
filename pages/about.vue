821b40ebb289b2da38fbdeb51b21478e8326f249
Jose <jlcaclosada@gmail.com>
2024-12-11 20:51:41 +0100

    First commit

---
+++ pages/about.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <h2>About</h2>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Hic laborum, nesciunt quos officiis in necessitatibus minus eum nostrum veniam iure.</p>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Hic laborum, nesciunt quos officiis in necessitatibus minus eum nostrum veniam iure.</p>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+h2 {
+    margin: 20px;
+    font-size: 36px;
+}
+
+p {
+    margin: 20px;
+    font-size: 18px;
+}
+
+</style>
